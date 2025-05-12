12b62226217a3aa0a0408f97c02b20fe7fa8210f
moonfeather <moonbite233@gmail.com>
2025-05-12 22:41:17 +0800

    cmake: Refactor heap size initialization and finalization logic

--- cmake/modules/OF.cmake
+++ cmake/modules/OF.cmake
@@ -1,12 +1,6 @@
 set(OF_TOTAL_HEAP_SIZE 0 CACHE INTERNAL "累计的堆内存需求")
 function(one_framework_init)
-    # 添加基础堆大小(可选)
-    math(EXPR FINAL_HEAP_SIZE "${OF_TOTAL_HEAP_SIZE} + 1024")
-
-    message(STATUS "设置最终堆大小: ${FINAL_HEAP_SIZE} 字节")
-
-    zephyr_compile_definitions(CONFIG_HEAP_MEM_POOL_SIZE=${FINAL_HEAP_SIZE})
-
+    one_framework_finalize_heap_size()
 endfunction()
 include(${CMAKE_CURRENT_LIST_DIR}/OFUnit.cmake)
 message(STATUS "-- One Framework build system activated --")
--- cmake/modules/OFUnit.cmake
+++ cmake/modules/OFUnit.cmake
@@ -43,3 +43,36 @@ function(one_framework_register_unit UNIT_HEADER)
     endif ()
 
 endfunction()
+
+function(one_framework_finalize_heap_size)
+    # 添加基础堆大小
+    math(EXPR FINAL_HEAP_SIZE "${OF_TOTAL_HEAP_SIZE} + 1024")
+
+    message(STATUS "设置最终堆大小: ${FINAL_HEAP_SIZE} 字节")
+
+    # 检查是否已经定义了 CONFIG_HEAP_MEM_POOL_SIZE
+    execute_process(
+            COMMAND ${CMAKE_C_COMPILER} -dM -E -x c /dev/null
+            COMMAND grep -q CONFIG_HEAP_MEM_POOL_SIZE
+            RESULT_VARIABLE NOT_DEFINED
+            OUTPUT_QUIET
+            ERROR_QUIET
+    )
+
+    if (NOT_DEFINED EQUAL 0)
+        # 如果已定义，获取当前值并加上 FINAL_HEAP_SIZE
+        execute_process(
+                COMMAND ${CMAKE_C_COMPILER} -dM -E -x c /dev/null
+                COMMAND grep CONFIG_HEAP_MEM_POOL_SIZE
+                OUTPUT_VARIABLE CURRENT_DEF
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        string(REGEX REPLACE "^#define CONFIG_HEAP_MEM_POOL_SIZE ([0-9]+).*$" "\\1" CURRENT_SIZE "${CURRENT_DEF}")
+        math(EXPR NEW_SIZE "${CURRENT_SIZE} + ${FINAL_HEAP_SIZE}")
+        message(STATUS "增加现有堆大小: ${CURRENT_SIZE} + ${FINAL_HEAP_SIZE} = ${NEW_SIZE} 字节")
+        zephyr_compile_definitions(CONFIG_HEAP_MEM_POOL_SIZE=${NEW_SIZE})
+    else ()
+        # 如果未定义，直接设置
+        zephyr_compile_definitions(CONFIG_HEAP_MEM_POOL_SIZE=${FINAL_HEAP_SIZE})
+    endif ()
+endfunction()
