d38cd8faae2807a9561919470f06573f32263f72
MoonFeather <moonbite233@gmail.com>
2025-05-16 17:21:41 +0800

    app: Initialize units at startup

--- tests/lib/applications/PRTS/src/main.cpp
+++ tests/lib/applications/PRTS/src/main.cpp
@@ -1,8 +1,10 @@
 #include "OF/lib/applications/PRTS/PrtsManager.hpp"
+#include "OF/lib/applications/Unit/Unit.hpp"
 #include "zephyr/kernel.h"
 
 [[noreturn]] int main()
 {
+    OF::StartUnits();
     OF::Prts::PrtsManager::initShell();
     while (true)
     {
