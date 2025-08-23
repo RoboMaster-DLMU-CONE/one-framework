a1d24a49a0723bfbce4ed04d90566e815e5f0ff3
MoonFeather <moonbite233@gmail.com>
2025-08-23 15:57:01 +0800

    add usb settings for dji board c

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,11 +3,13 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/usb/usbd.h>
 
 LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
 int main()
 {
+
     const struct device* status_led_dev = DEVICE_DT_GET(DT_NODELABEL(status_leds));
     if (!device_is_ready(status_led_dev))
     {
@@ -18,18 +20,13 @@ int main()
 
     led_api->set_heartbeat(status_led_dev);
 
-    const struct device* accel = DEVICE_DT_GET(DT_NODELABEL(bmi088_accel));
-    const struct device* gyro = DEVICE_DT_GET(DT_NODELABEL(bmi088_gyro));
-    if (!device_is_ready(accel) || !device_is_ready(gyro))
-    {
-        LOG_ERR("BMI088 devices not ready");
-        return -1;
-    }
+
     LOG_INF("程序开始运行……");
 
     while (true)
     {
-        k_sleep(K_MSEC(100));
+        k_sleep(K_MSEC(1000));
+        printk("Hello\n");
     }
 
     return 0;
